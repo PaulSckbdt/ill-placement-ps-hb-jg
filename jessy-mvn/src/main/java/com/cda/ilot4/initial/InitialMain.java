b7e8fc4d54a2ec985d2ebcf049feea7b2277b50f
jessy.guillemot <jessy.guillemot@hotmail.fr>
2021-02-16 11:28:46 +0100

    dependance email

--- jessy-mvn/src/main/java/com/cda/ilot4/initial/InitialMain.java
+++ jessy-mvn/src/main/java/com/cda/ilot4/initial/InitialMain.java
@@ -7,7 +7,7 @@ import org.apache.commons.mail.SimpleEmail;
 public class InitialMain {
 	
 	private static final String HOST = "smtp.gmail.com";
-    private static final int PORT = 25;
+    private static final int PORT = 587;
     private static final boolean SSL_FLAG = true;
 
 		public static void main(String [] args) {    
@@ -36,4 +36,6 @@ public class InitialMain {
 	            System.out.println(ex);
 	        }
 	    }
+
 }
+
